ed316020d3feab83e1b7d95ced77f6a79d1713e8
Alex <example@gmail.com>
2020-03-01 17:23:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+
+
+
+
+// console.log(leftBorderWidth);
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// let a = 5;
+// console.log(a);
+
+
+// console.log(4/0);
+// let somethinhgh;
+// console.log(somethinhgh);
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(persone["name"]);
+
+//let arr = ['plum.png','orange.jpg','apple.bmp'];
+//console.log(arr[0]);
+
+//alert("Hello World");
+
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+// let answer = +prompt("Are you 18?", "Yes");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" === 2);
+
+// let isChecked = false,
+//     isClose = false;
+
+//     console.log(isChecked || !isClose);
